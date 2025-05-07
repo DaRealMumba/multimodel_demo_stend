dd5f7eab935e543507505e0207518553f8358288
Muminsho Abdulvasiev <muminshoam@gmail.com>
2025-05-07 12:44:48 +0200

    change naming

--- src/app.py
+++ src/app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 pages = [
     st.Page("pages/main.py", title="Main"),
-    st.Page("pages/1_client_profile.py", title="Client Profile"),
-    st.Page("pages/2_portfolio.py", title="Portfolio"),
+    st.Page("pages/client_profile.py", title="Client Profile"),
+    st.Page("pages/portfolio.py", title="Portfolio"),
 ]
 
 pg = st.navigation(pages)
